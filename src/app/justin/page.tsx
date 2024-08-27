b4698d19824a44a2caed2df20500e88be1d48707
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-08-27 21:55:53 +0800

    hotfix - escape char

---
+++ src/app/justin/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Justin() {
+  return (
+    <main className="min-h-screen">
+      <h1>This is the justin page</h1>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <h1>Hello I'm Nicholas</h1>
+      <h1>Hello I am Nicholas</h1>
     </main>
   );
 }
